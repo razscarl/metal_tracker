f7189a48288271a0437623af3af354e6f3d34f52
Razscarl <razscarl@gmail.com>
2026-08-14 15:53:52 +1000

    v0.6.5 — analytics scaffold components, investment guide + movement arrow fixes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"3452d735bd38224ef2db85ca763d862d6326b1
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1297243489" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3014526835" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
